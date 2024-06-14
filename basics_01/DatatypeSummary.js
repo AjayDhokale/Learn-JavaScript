6dad3ebefb6f3174db68e1780a746b3d93b21b42
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-14 11:07:27 +0000

    DataTypes Summaray Stack  And Heap

--- basics_01/DatatypeSummary.js
+++ basics_01/DatatypeSummary.js
@@ -12,7 +12,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 // const bigNumber = 3456543576654356754n
 
@@ -32,6 +32,40 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+//----------------------------------------------------------------------------------------------
+
+
+//Stack (Premitive), Heap(non Premitive)
+
+let fname = "Ajay"
+let Lname = fname
+
+//console.log(Lname);
+
+Lname = "Anuradha"
+
+//console.log(fname);
+//console.log(Lname);       // In stack only gives the copy of values 
+
+
+let userOne ={
+    email: "ajay@gmail.com",
+    upi: "ajay@ybl"
+}
+
+
+let userTwo= userOne
+
+console.log(userOne);       //{ email: 'ajay@gmail.com', upi: 'ajay@ybl' }
+console.log(userTwo);       //{ email: 'ajay@gmail.com', upi: 'ajay@ybl' }
+
+
+userTwo.email="anuradha@gmail.com"   // In Heap gives the Original file of values 
+
+console.log(userOne);       //{ email: 'anuradha@gmail.com', upi: 'ajay@ybl' }
+console.log(userTwo);       //{ email: 'anuradha@gmail.com', upi: 'ajay@ybl' }

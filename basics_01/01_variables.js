8eb94649c0ee179270ca4bd784e804fabc8087a2
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-14 06:29:37 +0000

    variables done

---
+++ basics_01/01_variables.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+const accountId = "0081"
+let accountEmail = "ajaydhokale1508@gmail.com"
+var accountpass = "12345"
+accountCity = "Ichalkaranji"
+
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountpass,accountCity]);

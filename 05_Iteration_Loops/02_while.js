b73b39e9745e41ebf8ad8c0635b7db2eb987d09c
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-18 12:48:26 +0000

    while in js

---
+++ 05_Iteration_Loops/02_while.js
@@ -0,0 +1,48 @@
+///While Loop 
+
+//----------------------------------------- Method 1 ---------------------------------------
+
+// let index =0
+// let j =1
+// while (index < 20) {
+    
+//     index = index +2
+//     //console.log(`the Value of index is ${index}`);
+//     //console.log(index);
+// }
+
+
+//----------------------------------------- Method 2 ---------------------------------------
+
+
+// while (index <= 10) {
+    
+//     if (index == 2) {
+//         for (let i = 1; i <= 10; i++) {
+//             //console.log(index + " * " + i + " = " + index*i);
+//         }
+            
+//     }
+    
+//     index++
+// }
+
+//----------------------------------------- Method 2 ---------------------------------------
+
+
+let arr = ["ajay", "Anu", "kk" , "Sakshi"]
+let i= 0
+while (i < arr.length ) {
+    //console.log(arr[i]);
+    i++
+}
+
+
+//----------------------------------------- Method 2 ---------------------------------------
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);

3ce31ac0ed354a95c1d8889b67329931cb9222e1
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-19 07:48:15 +0000

    For_each Done in js

---
+++ 05_Iteration_Loops/05_for_Each.js
@@ -0,0 +1,49 @@
+// For Each
+
+
+const coding = ["js","java","cpp","ruby","python"]
+
+coding.forEach(function(val) {
+    //console.log(val);
+} )
+
+coding.forEach( (val2) => {
+    //console.log("Programming Languages are:", val2);
+} )
+
+
+function printme(item){
+   // console.log(item);
+}
+coding.forEach(printme)
+
+
+
+coding.forEach( (item,index, arr ) => {
+    //console.log(item,index,arr);
+} )
+
+
+const arr1 = [
+    {
+        name: "Ajay",
+        email: "ajay@gmail.com"
+    },
+    {
+        name: "Anu",
+        email: "anu@gmail.com"
+    },
+    {
+        name: "kk",
+        email: "kk@gmail.com"
+    },
+    {
+        name: "sakshi",
+        email: "sakshi@gmail.com"
+    }
+]
+
+
+arr1.forEach( (num1) => {
+    console.log(num1.name, " : ", num1.email);
+} )

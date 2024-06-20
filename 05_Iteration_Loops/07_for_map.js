ce0ff89ff91a4939d0fd1a9038be443d2e5fed8b
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-20 10:05:43 +0000

    map in loop donw

---
+++ 05_Iteration_Loops/07_for_map.js
@@ -0,0 +1,12 @@
+// map in iterations or loops
+
+const nums = [1,2,3,4,5,6,7,8,9,10]
+
+//let newNums = nums.map( (item) => { return item + 10} )
+
+let newNums = nums  
+                .map( (num1) => {return num1 * 10} )
+                .map( (num1) => { return num1 + 2} )
+                .filter( (num1) => {return num1 >= 40} )
+
+console.log(newNums);

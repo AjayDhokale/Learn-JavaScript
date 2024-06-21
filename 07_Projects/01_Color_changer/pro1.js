acdeb966a281d0ea9be5cd22eb6f02854a1f4544
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-21 12:40:47 +0000

    1

---
+++ 07_Projects/01_Color_changer/pro1.js
@@ -0,0 +1,11 @@
+const buttons = document.querySelectorAll(".button")
+const body = document.querySelector("body")
+
+buttons.forEach( function (button) {
+    console.log(button);
+    button.addEventListener("click", function(e){
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id
+        }
+    })
+});

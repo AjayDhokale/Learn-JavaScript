ea1303e975c11bc450fd8a8328d97a85b1310bfa
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-21 17:19:27 +0000

    project 1 and project 2 done

--- 07_Projects/01_Color_changer/pro1.js
+++ 07_Projects/01_Color_changer/pro1.js
@@ -1,25 +1,21 @@
-console.log("hitesh")
-const buttons = document.querySelectorAll('.button');
-const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
 
-buttons.forEach(function (button) {
-  console.log(button);
-  button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id === 'red') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'orange') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
-    
-  });
+buttons.forEach(function(button) {
+    button.addEventListener("click",function(e){
+        console.log(e);
+        console.log(e.target.id);
+        if (e.target.id === 'red') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'orange') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id
+        }
+    })
 });
-

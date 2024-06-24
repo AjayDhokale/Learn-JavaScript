4bb2fa8b7eba796d300433d1a72b03765276f220
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-24 09:47:12 +0000

    project 2 done

--- 07_Projects/02-BMI_Calculator/pro2.js
+++ 07_Projects/02-BMI_Calculator/pro2.js
@@ -3,35 +3,43 @@ const form = document.querySelector('form')
 form.addEventListener('click', function(e){
     e.preventDefault();
 
-    const height = parseInt(document.querySelector('height').value)
-    const weight = parseInt(document.querySelector('weight').value)
-    const result = document.querySelector('results')
-
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#results')
+    
     if (height === 0  || height < 0 || isNaN(height)  ) {
-        result.innerHTML = `Enter the valid height ${height}`
+        result.innerHTML = `Enter the valid height ${height}`;
     }else if (weight === 0  || weight < 0 || isNaN(weight)  ) {
-        result.innerHTML = `Enter the valid height ${weight}`
+        result.innerHTML = `Enter the valid height ${weight}`;
     }else {
-       const bmi = ((weight / (height*height)/1000)).toFixed(2);
-       result.innerHTML = `<span>${bmi}<span>`
-    }
-})
+      const  bmi = (weight / ((height * height) / 10000)).toFixed(2);
+       result.innerHTML = `<span>${bmi}<span>`;
 
-const para1 = document.querySelectorAll('p:nth-child(1)')
-const para2 = document.querySelectorAll('p:nth-child(2)')
-const para3 = document.querySelectorAll('p:nth-child(3)')
+        
+    if (bmi <= 18.6 ) {   
+        document.querySelector('#criteria').innerHTML= "Under Weight = Less than 18.6"
+    
 
-if (result < 18.6 ) {
-    para1.style.visibility='visible'
-    para2.style.visibility='hidden'
-    para3.style.visibility='hidden'
-}else if(result > 18.6 || result < 24.9){
-    para1.style.visibility='hidden'
-    para2.style.visibility='visible'
-    para3.style.visibility='hidden'
-}else if(result > 24.9){
-    para1.style.visibility='hidden'
-    para2.style.visibility='hidden'
-    para3.style.visibility='visible'
-}
+   }else if(bmi > 18.6 && bmi <= 24.9){
+       
+    
+     document.querySelector('#criteria').innerHTML=  "Normal Range = 18.6 and 24.9"
+   
+     
+   }else {
+      
+   
+     document.querySelector('#criteria').innerHTML=  "Overweight = Greater than 24.9"
+    
+     
+   }
+ }
+
+});
+
+// const para1 = document.querySelectorAll('p:nth-child(1)')
+// const para2 = document.querySelectorAll('p:nth-child(2)')
+// const para3 = document.querySelectorAll('p:nth-child(3)')
+
+
 

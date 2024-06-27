1b3ad9ff0fe12f6cd9924fa7df92f1aed5cdb6b7
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-06-27 11:15:30 +0000

    promises in js

--- 09_Advanced_One_js/02_promises.js
+++ 09_Advanced_One_js/02_promises.js
@@ -1,39 +1,126 @@
 // Promise(function(){}).then().then().catch().finally()          //syntax for Promise  //it is like fetch() method
 
-const promiseOne = new Promise(function(resolve,reject) {
-     //Do an async task
-    // DB calls, cryptography, network
+// const promiseOne = new Promise(function(resolve,reject) {
+//      //Do an async task
+//     // DB calls, cryptography, network
 
-    setTimeout(function() {
-        console.log("async task completed");
-        resolve()
-    },1000)
-});
-
-
-promiseOne.then(() => {
-    console.log("Promise Consumed...");
-})
+//     setTimeout(function() {
+//         console.log("async task completed");
+//         resolve()
+//     },1000)
+// });
 
 
-
-new Promise(function(resolve,reject) {
-    setTimeout(function() {
-        console.log("async task 2 completed ");
-        resolve()
-    },1000)
-}).then(() => console.log("promise 2 comsumed..."))
+// promiseOne.then(() => {
+//     console.log("Promise Consumed...");
+// })
 
 
 
-const promiseThree = new Promise((resolve,reject) => {
-    setTimeout(()=> {
-        console.log("task 3 ");
-        resolve({username: "Ajay",email: "ajay@gmail.com"})
-    },1000)
-})
+// new Promise(function(resolve,reject) {
+//     setTimeout(function() {
+//         console.log("async task 2 completed ");
+//         resolve()
+//     },1000)
+// }).then(() => console.log("promise 2 comsumed..."))
 
 
-promiseThree.then((user) => {
-    console.log(user);
-})
+
+// const promiseThree = new Promise((resolve,reject) => {
+//     setTimeout(()=> {
+//         console.log("Async task 3 completed ");
+//         resolve({username: "Ajay",email: "ajay@gmail.com"})
+//     },1000)
+// })
+
+
+// promiseThree.then((user) => {
+//     console.log(user);
+// })
+
+
+// const promiseFour = new Promise(function(resolve,reject) {
+//     setTimeout(function(){
+//         error = true
+//         if (!error) {
+//             console.log("Async task 4 completed ");
+//             resolve({username:"Ajay", password: "1234" })
+//         }else{
+//             reject("Error: Something went wrong...");
+//         }
+//     },1000)
+// })
+
+
+// promiseFour.then(function(user){
+//     console.log(user);
+//     return user.username
+// }).then(function(username){
+//     console.log(username);
+// }).catch(function(error){
+//     console.log(error);
+// }).finally(()=> console.log("The promise is either solved or rejected...."))
+
+
+
+
+// const promiseFive = new Promise(function(resolve,reject) {
+//     setTimeout(function() {
+//         error= false
+//         if (!error) {
+//             console.log("async Task 5 ......");
+//             resolve({username:"Ajay",email:"ajay@gmail.com"})
+//         }else{
+//             reject("Error : Somthing went wrong...")
+//         }
+//     },1000)
+// })
+
+
+// async function comsumePromiseFive() {
+//  try {
+//     const responce = await promiseFive
+//     console.log(responce);
+//  } catch (error) {
+//     console.log(error);
+//  }
+// }
+
+// comsumePromiseFive()
+
+
+
+// async function hiteshData() {
+// try {
+//     const responce = await fetch('https://api.github.com/users/hiteshchoudhary')
+
+//     const data = await responce.json()
+//     console.log(data);
+
+// } catch (error) {
+//     console.log("E: ",error);
+// } 
+
+// }
+
+// hiteshData()
+
+
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then(function(user){  
+    return user.json()
+}).then(function(info){
+    console.log(info);
+    return info.name
+}).then(function(nm){
+    console.log(nm);
+}).catch(function(error){
+    console.log(error);
+}).finally(()=> console.log("API fetched succesfully.........."))
+
+
+
+
+
+

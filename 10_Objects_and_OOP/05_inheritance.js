7bc349434091e0d837954555d84c471047484136
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-07-02 13:28:14 +0530

    inheritance in js done

--- 10_Objects_and_OOP/05_inheritance.js
+++ 10_Objects_and_OOP/05_inheritance.js
@@ -1,3 +1,37 @@
-console.log("hello");
+class user{
+    constructor(username){
+        this.username = username
+    }
+    
 
-console.log("hello");
+    logMe(){
+        console.log(`Username is ${this.username}`);
+    }
+}
+
+
+class Teacher extends user {
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password 
+    }
+
+    addCourse(){
+        console.log(`The new couse added by ${this.username}`);
+    }
+
+}
+
+const user1 = new Teacher("Ajay", "Ajay@gmail.com", 1234)
+
+// user1.logMe()
+
+const user2 = new Teacher("Anu")
+
+// user2.logMe()
+
+user1.addCourse()
+
+
+console.log(user1 instanceof user);

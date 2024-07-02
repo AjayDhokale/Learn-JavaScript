7c4f785a49741aba3a50f48eff904ab7ff010993
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-07-02 14:55:51 +0530

    classes in js done

---
+++ 10_Objects_and_OOP/05_myClasses.js
@@ -0,0 +1,25 @@
+
+
+class user {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email ;
+        this.password = password;   
+    }
+
+    encryptPassword() {
+        return `${this.password}abc`
+    }
+
+    changeUsername() {
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+
+
+const user1 = new user("Anu", "anu@gmail.com", 1234)
+
+console.log(user1.encryptPassword())
+console.log(user1.changeUsername())
+

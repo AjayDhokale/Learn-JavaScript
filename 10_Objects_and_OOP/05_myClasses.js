beac14efbcddda5cbc1d8c22304f75df6c206848
AjayDhokale <142399871+AjayDhokale@users.noreply.github.com>
2024-07-02 15:10:06 +0530

    my classes done in js

--- 10_Objects_and_OOP/05_myClasses.js
+++ 10_Objects_and_OOP/05_myClasses.js
@@ -20,6 +20,28 @@ class user {
 
 const user1 = new user("Anu", "anu@gmail.com", 1234)
 
-console.log(user1.encryptPassword())
-console.log(user1.changeUsername())
+// console.log(user1.encryptPassword())
+// console.log(user1.changeUsername())
 
+
+// behind the scene
+
+
+function UserData(username,email , password){
+    this.username = username
+    this.email = email
+    this.password = password
+}
+
+UserData.prototype.encryptPassword = function() {
+    console.log(`abcd${this.password}abc`);
+}
+
+UserData.prototype.changeUsername = function () {
+    console.log(`${this.username.toUpperCase()}`);
+}
+
+const student = new UserData("Anuradha", "anu@gmail.com", 12345)
+
+student.encryptPassword()
+student.changeUsername()
